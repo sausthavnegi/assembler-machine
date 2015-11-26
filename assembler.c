a257b61282a6c5a7370977c03b9a657bed560aa0
yamini61 <yaminimaheshwari6@gmail.com>
2015-11-26 00:54:41 -0800

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -1,4 +1,4 @@
-# assembler-machine
+#assembler-machine
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
@@ -6,58 +6,50 @@ void main()
 {
   //pass 1 initial
     FILE *f1,*f2,*f3,*f4,*f5;
-    int lc,sa,l,op1,o,lan,k;
-    char m1[20],la[20],op[20],otp[20];
-    
+    int lc,sa,l,op1,o,lan,n=0;
+    char opnd[20],la[20],op[20],otp[20];
+  
   //pass 1 start
 
   f1=fopen("INPUT.DAT","r");
   f3=fopen("SYMTABLE.DAT","w");
   f5=fopen("INTERMED.DAT","w");
-  fscanf(f1,"%s %s %d",la,m1,&op1);
-     if(strcmp(m1,"START")==0)
-      {
+  fscanf(f1,"%s %s %d",la,opnd,&op1);
+   printf(" INPUT PROGRAM INTO OBJECT CODE\n");
+   printf("----------------------------------------\n");
+     if(strcmp(opnd,"START")==0)
+     {
          sa=op1;
          lc=sa;
-         printf("\t%s\t%s\t%d\n",la,m1,op1);
-         fprintf(f5,"\t%s\t%s\t%d\n",la,m1,op1);
+
+         printf("address\tlabel\topcode\toperand\n\n");
+         printf("\t%s\t%s\t%d\n",la,opnd,op1);
+         fprintf(f5,"\t%s\t%s\t%d\n",la,opnd,op1);
       }
     else
          lc=0;
-  fscanf(f1,"%s %s",la,m1);
+  fscanf(f1,"%s %s",la,opnd);
  while(!feof(f1))
   {
        fscanf(f1,"%s",op);
-       printf("\n%d\t%s\t%s\t%s\n",lc,la,m1,op);
-       fprintf(f5,"\n%d\t%s\t%s\t%s\n",lc,la,m1,op);
-       if(strcmp(la,"-")!=0)
+       printf("\n%d\t%s\t%s\t%s\n",lc,la,opnd,op);
+       fprintf(f5,"\n%d\t%s\t%s\t%s\n",lc,la,opnd,op);
+       if(strcmp(la,"**")!=0)
          {
                   fprintf(f3,"\n%d\t%s\n",lc,la);
          }
-       f2=fopen("OPTAB.DAT","r");
-       fscanf(f2,"%s %d",otp,&o);
-       while(!feof(f2))
-          {
-              if(strcmp(m1,otp)==0)
-                {
-                        lc=lc+3;
-                        break;
-                }
-              fscanf(f2,"%s %d",otp,&o);
-        }
 
-    fclose(f2);
 
-    if(strcmp(m1,"WORD")==0)
+    if(strcmp(opnd,"WORD")==0)
       {
            lc=lc+3;
       }
-    else if(strcmp(m1,"RESW")==0)
+    else if(strcmp(opnd,"RESW")==0)
       {
        op1=atoi(op);
        lc=lc+(3*op1);
       }
-    else if(strcmp(m1,"BYTE")==0)
+    else if(strcmp(opnd,"BYTE")==0)
       {
           if(op[0]=='X')
                 lc=lc+1;
@@ -67,21 +59,34 @@ void main()
                    lc=lc+lan;
              }
       }
-    else if(strcmp(m1,"RESB")==0)
+    else if(strcmp(opnd,"RESB")==0)
      {
          op1=atoi(op);
-
-            lc=lc+op1;
+          lc=lc+op1;
      }
+     else
+     {
+       f2=fopen("OPTAB.DAT","r");
+       fscanf(f2,"%s %d",otp,&o);
+       while(!feof(f2))
+          {
+              if(strcmp(opnd,otp)==0)
+                {
+                        lc=lc+3;
+                        break;
+                }
+              fscanf(f2,"%s %d",otp,&o);
+           }
 
-    fscanf(f1,"%s%s",la,m1);
+        fclose(f2);
+      }
+
+    fscanf(f1,"%s%s",la,opnd);
   }
-    if(strcmp(m1,"END")==0)
+    if(strcmp(opnd,"END")==0)
       {
         printf("TOTAL LENGTH =%d\n",lc-sa);
       }
     fclose(f1);
     fclose(f3);
     fclose(f5);
-
-}
